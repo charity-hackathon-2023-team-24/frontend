b77cde4e3ebc2623a7debee075e6b43586e142c8
Elena Shcherbinina <el.shcherbinina@outlook.com>
2023-11-12 16:12:33 +0300

    updated homepage

--- src/components/pages/Home/HomePage.jsx
+++ src/components/pages/Home/HomePage.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 import styles from './HomePage.module.css';
+import boy from '../../../assets/boy-2.png';
+import girl from '../../../assets/girl.png';
 import StartButton from '../../ui/main/StartButton/StartButton';
 import Rules from '../../ui/main/Rules/Rules';
 import Modal from '../../modal/Modal';
@@ -13,6 +15,11 @@ const HomePage = () => {
     <main className={styles.container}>
       {!modal ? (
         <div className={styles.homeContent}>
+          <img className={styles.girlImg} src={girl} alt='girl' />
+          <img className={styles.boyImg} src={boy} alt='boy' />
+          <p>
+            Победи свои страхи <br /> и найди новую семью!
+          </p>
           <StartButton />
           <Rules />
         </div>
